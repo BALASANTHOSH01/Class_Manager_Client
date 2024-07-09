ec9b1ccefc47b7a139a75cc76e7c83f8d2638ca1
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-09 23:28:22 +0530

    login page updation

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_END_POINT;
 
-console.log("API Key: " + API_KEY);
-
 const axiosInstance = axios.create({
   baseURL: API_KEY,
   headers: {
@@ -15,12 +13,10 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // You can modify the request configuration here if needed
-    console.log("Request:", config);
     return config;
   },
   (error) => {
     // Handle request error here
-    console.error("Request Error:", error);
     return Promise.reject(error);
   }
 );
@@ -29,12 +25,10 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => {
     // Handle the response data here if needed
-    console.log("Response:", response);
     return response;
   },
   (error) => {
     // Handle response error here
-    console.error("Response Error:", error);
     return Promise.reject(error);
   }
 );
